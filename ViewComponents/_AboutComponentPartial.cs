d98f0486a0fdb33752656c6b93f96c850accdffc
Merve Çavuşoğlu <merwecvss@gmail.com>
2025-08-25 14:58:44 +0300

    About kısmını dinamik hale getirip verileri tablodan foreach ile çektim.

--- ViewComponents/_AboutComponentPartial.cs
+++ ViewComponents/_AboutComponentPartial.cs
@@ -1,11 +1,17 @@
 using Microsoft.AspNetCore.Mvc;
+using my_portfolio.Models.Context;
 
 namespace my_portfolio.ViewComponents
 {
     public class _AboutComponentPartial : ViewComponent
     {
+        DataContext portfolioContext = new DataContext();
         public IViewComponentResult Invoke()
         {
+            ViewBag.aboutTitle = portfolioContext.Abouts.Select(x => x.Title).FirstOrDefault();
+            ViewBag.aboutSubDescription = portfolioContext.Abouts.Select(x => x.SubDescription).FirstOrDefault();
+            ViewBag.aboutDetail = portfolioContext.Abouts.Select(x => x.Details).FirstOrDefault();
+
             return View();
         }
     }
