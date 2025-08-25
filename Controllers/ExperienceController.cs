c6ee18aaa56559a0be0909c686f0490e44aaec9a
Merve Çavuşoğlu <merwecvss@gmail.com>
2025-08-25 17:14:25 +0300

    Admin panelinde deneyimler kısmında görünüş oluşturdum.

--- Controllers/ExperienceController.cs
+++ Controllers/ExperienceController.cs
@@ -1,12 +1,15 @@
 using Microsoft.AspNetCore.Mvc;
+using my_portfolio.Models.Context;
 
 namespace my_portfolio.Controllers
 {
     public class ExperienceController : Controller
     {
+        DataContext context = new DataContext();
         public IActionResult ExperienceList()
         {
-            return View();
+            var values = context.Experiences.ToList();
+            return View(values);
         }
     }
 }
