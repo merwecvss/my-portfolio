3956d68f67d0be68e0e9432b55cfd9b4b62ead50
Merve Çavuşoğlu <merwecvss@gmail.com>
2025-08-26 18:24:10 +0300

    ToDoList kısımlarını yaptım.

--- ViewComponents/LayoutViewComponents/_LayoutNavbarComponentPartial.cs
+++ ViewComponents/LayoutViewComponents/_LayoutNavbarComponentPartial.cs
@@ -1,12 +1,16 @@
 using Microsoft.AspNetCore.Mvc;
+using my_portfolio.Models.Context;
 
 namespace my_portfolio.ViewComponents.LayoutViewComponents
 {
     public class _LayoutNavbarComponentPartial : ViewComponent 
     {
+        DataContext context = new DataContext();
         public IViewComponentResult Invoke()
         {
-            return View();
+            ViewBag.toDoListCount = context.ToDoLists.Where(XmlConfigurationExtensions => XmlConfigurationExtensions.Status == false).Count();
+            var values = context.ToDoLists.Where(XmlConfigurationExtensions => XmlConfigurationExtensions.Status == false).ToList();
+            return View(values);
         }
     }
 }
