17fa48761de0c90f8653de7344eb1e34dccc5a4a
Merve Çavuşoğlu <merwecvss@gmail.com>
2025-08-27 16:50:09 +0300

    Portfolio kısmı dinamik hale getirildi.

--- ViewComponents/_PortfolioComponentPartial.cs
+++ ViewComponents/_PortfolioComponentPartial.cs
@@ -1,12 +1,15 @@
 using Microsoft.AspNetCore.Mvc;
+using my_portfolio.Models.Context;
 
 namespace my_portfolio.ViewComponents
 {
     public class _PortfolioComponentPartial : ViewComponent
     {
+        DataContext portfolioContext = new DataContext();
         public IViewComponentResult Invoke()
         {
-            return View();
+            var values = portfolioContext.Portfolios.ToList();
+            return View(values);
         }
     }
 }
